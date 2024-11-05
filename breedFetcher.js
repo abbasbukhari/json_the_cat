3f8e00b4ae4eed6ed762b020874406da97f8b83d
abbas <abbasbukhari2015@gmail.com>
2024-11-05 12:03:19 -0500

    Refactored breedFetcher. added tests with Mocha & Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
+// breedFetcher.js
 const needle = require("needle");
 
-const fetchBreedDescription = (breedName) => {
+const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  needle.get(url, (error, response) => {
+  needle.get(url, (error, response, body) => {
     if (error) {
-      console.error("Error fetching breed data:", error);
+      callback(error, null); // Pass error to callback
       return;
     }
 
-    const data = response.body;
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+    const data = JSON.parse(body);
+    const breed = data[0];
+
+    if (breed) {
+      callback(null, breed.description);
     } else {
-      console.log("Description:", data[0].description);
+      callback("Breed not found", null);
     }
   });
 };
 
-const breedName = process.argv[2];
-fetchBreedDescription(breedName);
+module.exports = { fetchBreedDescription };
